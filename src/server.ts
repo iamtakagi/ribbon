5a50e030a3e6f861201244347acec28fcf0778b7
iamtakagi <github@iamtakagi.net>
2024-01-24 06:20:25 +0900

    src に移動

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,16 @@ import { Hono } from 'hono'
 import { env } from 'hono/adapter'
 import type { AppLoadContext } from '@remix-run/cloudflare'
 import { createRequestHandler } from '@remix-run/cloudflare'
-import * as build from './build'
+import * as build from '../build'
+import * as containerSetter from '../app/container/setter';
+import container from '../app/container'
 
 // @ts-ignore
 const handleRemixRequest = createRequestHandler(build, process.env.NODE_ENV)
 
 const app = new Hono()
+
+containerSetter.set(container);
 
 app.all('*', async (c) => {
   const loadContext: AppLoadContext = { env: env(c) }
