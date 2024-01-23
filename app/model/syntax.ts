6d2ed31051f1552f3509ac6367c36649c69d72eb
iamtakagi <github@iamtakagi.net>
2024-01-24 07:30:12 +0900

    実装見通し付かないのでので消す

--- app/model/syntax.ts
+++ app/model/syntax.ts
@@ -1,6 +1,5 @@
 export const Syntax = {
   Markdown: "Markdown",
-  Scrapbox: "Scrapbox",
 } as const;
-export const Syntaxes = [Syntax.Markdown, Syntax.Scrapbox] as const;
+export const Syntaxes = [Syntax.Markdown] as const;
 export type Syntax = (typeof Syntax)[keyof typeof Syntax];
