29825bdfbaf07ea1c97f4e286d097601f5012ff9
iamtakagi <github@iamtakagi.net>
2024-01-24 07:13:23 +0900

    rename

--- app/container/setter.ts
+++ app/container/setter.ts
@@ -1,11 +1,11 @@
 import type { Container } from 'inversify';
-import type { IArticleRepository, IArticleRepositoryFactory } from '../repository/types';
-import { ArticleRepositoryFactory } from '../repository/article';
+import type { IArticleD1Repository, IArticleD1RepositoryFactory } from '../repository/types';
+import { ArticleD1RepositoryFactory } from '../repository/article';
 
 export const set = (container: Container): void => {
-    container.bind<IArticleRepositoryFactory>('IArticleRepositoryFactory').to(ArticleRepositoryFactory).inSingletonScope();
-    container.bind<IArticleRepository>('IArticleRepository').toDynamicValue((context) => {
-        const factory = context.container.get<IArticleRepositoryFactory>('IArticleRepositoryFactory');
+    container.bind<IArticleD1RepositoryFactory>('IArticleRepositoryD1Factory').to(ArticleD1RepositoryFactory).inSingletonScope();
+    container.bind<IArticleD1Repository>('IArticleD1Repository').toDynamicValue((context) => {
+        const factory = context.container.get<IArticleD1RepositoryFactory>('IArticleD1RepositoryFactory');
         return factory.create();
     }).inSingletonScope();
 }
