f7b5d6f88bbec8831ad5547a8d13b6438857b61f
iamtakagi <github@iamtakagi.net>
2024-01-24 07:20:27 +0900

    feat: edit action

--- app/routes/edit.tsx
+++ app/routes/edit.tsx
@@ -1,13 +1,38 @@
 import {
   type MetaFunction,
   type LoaderFunction,
+  ActionFunction,
+  ActionFunctionArgs,
+  redirect,
 } from "@remix-run/cloudflare";
 import { handleRequireAuth } from "../components/authentication/handleRequireAuth";
 import { Editor } from "../components/editor/Editor";
+import { Scope } from "app/model/scope";
+import { Syntax } from "app/model/syntax";
+import { nanoid } from "nanoid";
+import container from "../container";
+import { IArticleD1Repository } from "app/repository/types";
 
 export const loader: LoaderFunction = async ({ request }) => {
   return await handleRequireAuth(request);
 };
+
+export const action: ActionFunction = async ({request, params }: ActionFunctionArgs) => {
+  const body = await request.formData();
+  const scope = body.get("scope");
+  const syntax = body.get("syntax");
+  const title = body.get("title");
+  const description = body.get("description");
+  const raw = body.get("raw");
+  if (scope == null || typeof scope !== 'string' || Scope[scope as keyof typeof Scope] == null) return redirect("/editor");
+  if (syntax == null || typeof syntax !== 'string' || Syntax[syntax as keyof typeof Syntax] == null) return redirect("/editor");
+  if (title == null || typeof title !== 'string') return redirect("/editor");
+  if (description != null && typeof description !== 'string') return redirect("/editor");
+  if (raw == null || typeof raw !== 'string') return redirect("/editor");
+  const articleRepository = container.get<IArticleD1Repository>('IArticleD1Repository');
+  const result = await articleRepository.upsertOne(nanoid(), scope as Scope, syntax as Syntax, title, description, raw);
+  if (result) return redirect(`/`);
+}
 
 export const meta: MetaFunction = () => {
   return [
